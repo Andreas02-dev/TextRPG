c9f251c627d3657a3d42dae192543a7a164c76ee
Andreas <andreashoornstra@gmail.com>
2020-08-06 16:05:11 +0200

    Added invalid selection and exit game functionality

---
+++ StartMenuSelection.java
@@ -0,0 +1,27 @@
+package textRPG;
+import java.util.Scanner;
+
+public class StartMenuSelection {
+	static Scanner keyboard = new Scanner(System.in);
+	public static void startMenuSelector() {
+		boolean repeat = true;
+		
+		while (repeat) {
+			
+			System.out.println("(1) Start new game");
+			System.out.println("(2) Exit game");
+			int startMenuSelect = keyboard.nextInt();
+			
+			switch (startMenuSelect) {
+			case 1: 
+					repeat = false;
+					break;
+			case 2: 
+					repeat = false;
+					break;
+			default: System.out.println("Invalid selection.");
+					 break;
+			}
+		}
+	}
+}
