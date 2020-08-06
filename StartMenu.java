ae878ead7a5d35500772eae5524ba78e254dc71f
Andreas <andreashoornstra@gmail.com>
2020-08-06 16:04:11 +0200

    Added StartMenuSelection method

--- StartMenu.java
+++ StartMenu.java
@@ -1,12 +1,12 @@
 package textRPG;
 
 public class StartMenu {
-	
+
 	public static void startMenu() {
 		System.out.println("Welcome to Auro's world!");
 		System.out.println("------------------------");
 		System.out.println("Please make your selection:");
-		startMenuSelection(keyboard);
+		StartMenuSelection.startMenuSelector();
 	}
-	
+
 }
