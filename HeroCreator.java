562e90096f06b4e860fa0da3b04958d38d894717
Andreas <andreashoornstra@gmail.com>
2020-08-07 17:26:02 +0200

    Added Archer hero to selection and advance to Game

---
+++ HeroCreator.java
@@ -0,0 +1,27 @@
+package textRPG;
+import java.util.Scanner;
+
+public class HeroCreator {
+	static Scanner keyboard = new Scanner(System.in);
+	static Heroes archie = new Archer("Archie", 1, 5, 20);
+	
+	//system variable
+	static Heroes hero;
+	static boolean notRepeat = false;
+	public static void heroCreator() {
+		System.out.println("----------------------------------------------");
+		System.out.println("Please choose your hero!");
+		System.out.println("(1) Archer");
+		int heroSelection = keyboard.nextInt();
+		while (!notRepeat) {
+			switch (heroSelection) {
+			case 1:	hero = archie;
+					Game.Dungeon(hero);
+					break;
+			default: System.out.println("Invalid option!");
+					notRepeat = true;
+					break;
+			}
+		}
+	}
+}
