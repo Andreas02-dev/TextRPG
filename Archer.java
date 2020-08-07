211ed9527bd73b05aded4979ffc5bb6bd930ec38
Andreas <andreashoornstra@gmail.com>
2020-08-07 17:23:42 +0200

    Archer dmg10 hp100 at lvl1

--- Archer.java
+++ Archer.java
@@ -1,7 +1,15 @@
 package textRPG;
 
 public class Archer extends Heroes {
+	String name;
+	int level;
+	int damageDealt;
+	int hitPoints;
 	public Archer(String name, int level, int damageDealt, int hitPoints) {
-		super("Archer", 1, 5, 20);
+		super("Archer", 1, 10, 100);
+		this.name = name;
+		this.level = level;
+		this.damageDealt = damageDealt;
+		this.hitPoints = hitPoints;
 	}
 }
