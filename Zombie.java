536395cb53e9fc13ed97476994c6b7f81e5f6c42
Andreas <andreashoornstra@gmail.com>
2020-08-07 17:21:48 +0200

    Zombie dmg5 and hp20 at lvl1

--- Zombie.java
+++ Zombie.java
@@ -1,7 +1,16 @@
 package textRPG;
+import java.util.Random;
 
 public class Zombie extends Monsters {
+	String name;
+	int level;
+	int damageDealt;
+	int hitPoints;
 	public Zombie(String name, int level, int damageDealt, int hitPoints) {
-		super("Zombie", 1, 2, 10);
+		super("Zombie", 1, 5, 20);
+		this.name = name;
+		this.level = level;
+		this.damageDealt = damageDealt;
+		this.hitPoints = hitPoints;
 	}
 }
