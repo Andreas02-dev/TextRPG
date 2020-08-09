d1ca1e9881b2630aa00b02c366a84b69aba201d8
Andreas <andreashoornstra@gmail.com>
2020-08-09 14:44:29 +0200

    Added healthPotionAmount

--- Heroes.java
+++ Heroes.java
@@ -6,11 +6,13 @@ public class Heroes {
 	int damageDealt;
 	int hitPoints;
 	int walletAmount;
-	public Heroes(String name, int level, int damageDealt, int hitPoints, int walletAmount) {
+	int healthPotionAmount;
+	public Heroes(String name, int level, int damageDealt, int hitPoints, int walletAmount, int healthPotionAmount) {
 		this.name = name;
 		this.level = level;
 		this.damageDealt = damageDealt;
 		this.hitPoints = hitPoints;
 		this.walletAmount = walletAmount;
+		this.healthPotionAmount = healthPotionAmount;
 	}
 }
