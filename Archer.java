c57d9f4158d773e6e5496f87dbd534b0d3fa198f
Andreas <andreashoornstra@gmail.com>
2020-08-09 14:43:42 +0200

    added healthPotionAmount

--- Archer.java
+++ Archer.java
@@ -6,12 +6,14 @@ public class Archer extends Heroes {
 	int damageDealt;
 	int hitPoints;
 	int walletAmount;
-	public Archer(String name, int level, int damageDealt, int hitPoints, int walletAmount) {
+	int healthPotionAmount;
+	public Archer(String name, int level, int damageDealt, int hitPoints, int walletAmount, int healthPotionAmount) {
 		super("Archer", 1, 10, 100, 0);
 		this.name = name;
 		this.level = level;
 		this.damageDealt = damageDealt;
 		this.hitPoints = hitPoints;
 		this.walletAmount = walletAmount;
+		this.healthPotionAmount = healthPotionAmount;
 	}
 }
