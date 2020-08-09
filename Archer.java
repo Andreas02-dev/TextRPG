c48a2babaa83c5fd61411dd114cf36b2f61e173f
Andreas <andreashoornstra@gmail.com>
2020-08-09 13:58:38 +0200

    Added walletAmount

--- Archer.java
+++ Archer.java
@@ -5,11 +5,13 @@ public class Archer extends Heroes {
 	int level;
 	int damageDealt;
 	int hitPoints;
-	public Archer(String name, int level, int damageDealt, int hitPoints) {
-		super("Archer", 1, 10, 100);
+	int walletAmount;
+	public Archer(String name, int level, int damageDealt, int hitPoints, int walletAmount) {
+		super("Archer", 1, 10, 100, 0);
 		this.name = name;
 		this.level = level;
 		this.damageDealt = damageDealt;
 		this.hitPoints = hitPoints;
+		this.walletAmount = walletAmount;
 	}
 }
