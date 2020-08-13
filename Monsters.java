e61ec9ebab2f3bb878b2ae19dfefd4084b3feced
Andreas <andreashoornstra@gmail.com>
2020-08-13 23:00:40 +0200

    Added more parameters for random number generator in Dungeon of Death

--- Monsters.java
+++ Monsters.java
@@ -3,12 +3,16 @@ package textRPG;
 public class Monsters {
 	String name;
 	int level;
-	int damageDealt;
-	int hitPoints;
-	public Monsters(String name, int level, int damageDealt, int hitPoints) {
+	int maxBaseDamageDealt;
+	int damagePerLevel;
+	int maxBaseHitPoints;
+	int hitPointsPerLevel;
+	public Monsters(String name, int level, int maxBaseDamageDealt, int damagePerLevel, int maxBaseHitPoints, int hitPointsPerLevel) {
 		this.name = name;
 		this.level = level;
-		this.damageDealt = damageDealt;
-		this.hitPoints = hitPoints;
+		this.maxBaseDamageDealt = maxBaseDamageDealt;
+		this.damagePerLevel = damagePerLevel;
+		this.maxBaseHitPoints = maxBaseHitPoints;
+		this.hitPointsPerLevel = hitPointsPerLevel;
 	}
 }
