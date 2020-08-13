9efa5e253bf7ad325d81822230ce9fd5c6a336c8
Andreas <andreashoornstra@gmail.com>
2020-08-13 23:03:35 +0200

    Added int array with leveling exp requirements for Heroes

--- TextRPG.java
+++ TextRPG.java
@@ -1,7 +1,14 @@
 package textRPG;
-public class TextRPG {
 
+public class TextRPG {
+	static int[] leveling = new int[100];
 	public static void main(String[] args) {
-		StartMenu.startMenu();
+		
+		double a = 500;
+		for (int i = 0; i<=99; i++) {
+			a = a * 1.081;
+			leveling[i] = (int) Math.round(a);
+		}
+	StartMenu.startMenu();
 	}
 }
